6de6f88bc494986b177feb8d071f71e4cdd942ac
Sharad M <codebysharad@gmail.com>
2026-07-10 14:40:25 +0530

    Online Gallary [Photo + Video Grid Complete]

--- OnlineGallary/src/components/ResultGrid.jsx
+++ OnlineGallary/src/components/ResultGrid.jsx
@@ -1,12 +1,12 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { fetch_Imgs, fetch_Vids } from "../api/mediaApi";
 import {
-  setQuery,
   setLoading,
   setError,
   setResults,
 } from "../redux/features/searchSlice";
 import { useDispatch, useSelector } from "react-redux";
+import ResultCard from "./ResultCard";
 
 const ResultGrid = () => {
     const dispatch = useDispatch()
@@ -16,43 +16,55 @@ const ResultGrid = () => {
 
   useEffect(
       function () {
+
+        if (!query) return
           const getData = async () => {
-            try{
+            try {
                 dispatch(setLoading())
-                let data;
-              if (activeTab == "Photos") {
-                let res = await fetch_Imgs("cat");
+                let data = [];
+              if (activeTab === "Photos") {
+                let res = await fetch_Imgs(query);
                 data = res.results.map((item) => ({
                   id: item.id,
                   type: "photo",
                   title: item.alt_description,
                   thumbnail: item.urls.small,
-                  src: item.urls.full
+                  src: item.urls.full,
                 }));
-              }
-              if (activeTab == "Videos") {
-                let res = await fetch_Vids("cat");
-                  data = res.results.map((item) => ({
-                      id: item.id,
-                      type: "Video",
-                      title: item.user.name || "Video",
-                      thumbnail: item.image,
-                      src: item.video_files[0].link
+              } else if (activeTab === "Videos") {
+                let res = await fetch_Vids(query);
+                data = res.videos.map((item) => ({
+                  id: item.id,
+                  type: "video",
+                  title: item.user.name || "Video",
+                  thumbnail: item.image,
+                  src: item.video_files[0]?.link,
+                  url: item.video_files[0]?.link,
                 }));
               }
               console.log(data)
               dispatch(setResults(data))
-            }catch(err){
-                dispatch(setError(err))
-                
+            } catch (err) {
+                dispatch(setError(err.message))
             }
-            }
+          }
       getData();
     },
-    [query, activeTab],
+    [query, activeTab, dispatch],
   );
 
-  return <></>;
+  if (error) return <h1>Error While Loading...</h1>;
+  if (loading) return <h1>Loading...</h1>;
+
+    return (
+        <div className='flex justify-between w-full flex-wrap gap-6 overflow-auto px-10'>
+            {results.map((item, idx) => {
+                return <div key={idx}>
+                    <ResultCard item={item} />
+                </div>
+            })}
+        </div>
+    )
 };
 
 export default ResultGrid;
